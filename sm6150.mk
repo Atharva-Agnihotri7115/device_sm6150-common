3ef9b6515b1a2febf5e133c1b9a94399e7963e09
baalajimaestro <me@baalajimaestro.me>
2022-02-04 23:27:43 +0300

    sm6150-common: Optimise dex flags

--- sm6150.mk
+++ sm6150.mk
@@ -108,6 +108,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_DEXPREOPT_SPEED_APPS += \
     SystemUIGoogle
 
+PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.composer@2.4-service \
