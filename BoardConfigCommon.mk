66f84394597b0bd5c379124dd5017059ffdd1696
unsatifsed27 <unsatifsederror@gmail.com>
2022-10-25 21:13:36 +0530

    sm6150-common: ignore neverallows for now

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -148,6 +148,8 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
+SELINUX_IGNORE_NEVERALLOWS := true
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
